bc4c9b8e3809e306ee2f5715ba00e78851a01c5f
Gustavo Alarcon <galarcon@meraki-s.com>
2020-11-21 02:00:28 -0500

    feature 1.1.56

---
+++ src/app/shared-dialogs/after19-dialog/after19-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-after19-dialog',
+  templateUrl: './after19-dialog.component.html',
+  styleUrls: ['./after19-dialog.component.scss']
+})
+export class After19DialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
