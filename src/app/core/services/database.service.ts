b85d066c5701c8c72738fa566201f73c0c0c632d
Gustavo Alarcon <galarcon@meraki-s.com>
2021-01-18 14:20:36 -0500

    gitflow-release-stash: 1.1.67

--- src/app/core/services/database.service.ts
+++ src/app/core/services/database.service.ts
@@ -31,7 +31,7 @@ import { AngularFireAuth } from "@angular/fire/auth";
   providedIn: "root",
 })
 export class DatabaseService {
-  public version: string = "V1.1.66r";
+  public version: string = "V1.1.67r";
   public isOpen: boolean = false;
   public isAdmin: boolean = false;
   public messageSaw: number = 0;
